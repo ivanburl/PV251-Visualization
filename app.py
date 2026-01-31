b386a1b85a78fa8e5ab86fa1bcfbc629f319f5ba
Burlustkyi Ivan <ivandr2004@gmail.com>
2026-01-31 19:40:14 +0100

    remove unrar and debug

--- app.py
+++ app.py
@@ -36,4 +36,5 @@ app.layout = dmc.MantineProvider(
 server = app.server
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=8050)
+    server = app.server
+    app.run(host='0.0.0.0', port=8050)
